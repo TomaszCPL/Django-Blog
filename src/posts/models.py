c5a6c1094c053f39aceef858c7a6d755838bc87f
TomaszCPL <czep.tomasz@gmail.com>
2017-12-14 15:05:28 +0100

    final version

--- src/posts/models.py
+++ src/posts/models.py
@@ -4,10 +4,20 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save
+from django.utils import timezone
 
 from django.utils.text import slugify
 # Create your models here.
 # MVC MODEL VIEW CONTROLLER
+
+
+#Post.objects.all()
+#Post.objects.create(user=user, title="Some time")
+
+class PostManager(models.Manager):
+    def active(self, *args, **kwargs):
+        # Post.objects.all() = super(PostManager, self).all()
+        return super(PostManager, self).filter(draft=False).filter(publish__lte=timezone.now())
 
 
 def upload_location(instance, filename):
@@ -19,16 +29,20 @@ class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
-    image = models.ImageField(upload_to=upload_location,
-            null=True,
-            blank=True,
-            width_field="width_field",
+    image = models.ImageField(upload_to=upload_location, 
+            null=True, 
+            blank=True, 
+            width_field="width_field", 
             height_field="height_field")
     height_field = models.IntegerField(default=0)
     width_field = models.IntegerField(default=0)
     content = models.TextField()
+    draft = models.BooleanField(default=False)
+    publish = models.DateField(auto_now=False, auto_now_add=False)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    objects = PostManager()
 
     def __unicode__(self):
         return self.title
@@ -63,6 +77,7 @@ def pre_save_post_receiver(sender, instance, *args, **kwargs):
 
 
 pre_save.connect(pre_save_post_receiver, sender=Post)
+
 
 
 
