cf9e7a625dc013d48b6aa4567322a832a2b58c9d
TomaszCPL <czep.tomasz@gmail.com>
2017-12-14 13:39:40 +0100

    added user permissions

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,16 +1,21 @@
+from urllib import quote_plus
+
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
-from urllib import quote_plus
+
 from .forms import PostForm
 from .models import Post
 
 def post_create(request):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
+		# message success
 		messages.success(request, "Successfully Created")
 		return HttpResponseRedirect(instance.get_absolute_url())
 	context = {
@@ -19,13 +24,12 @@ def post_create(request):
 	return render(request, "post_form.html", context)
 
 def post_detail(request, slug=None):
-
 	instance = get_object_or_404(Post, slug=slug)
 	share_string = quote_plus(instance.content)
 	context = {
 		"title": instance.title,
 		"instance": instance,
-		"share_string": share_string
+		"share_string": share_string,
 	}
 	return render(request, "post_detail.html", context)
 
@@ -45,7 +49,7 @@ def post_list(request):
 
 
 	context = {
-		"object_list": queryset, 
+		"object_list": queryset,
 		"title": "List",
 		"page_request_var": page_request_var
 	}
@@ -56,6 +60,8 @@ def post_list(request):
 
 
 def post_update(request, slug=None):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	instance = get_object_or_404(Post, slug=slug)
 	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 	if form.is_valid():
@@ -74,6 +80,8 @@ def post_update(request, slug=None):
 
 
 def post_delete(request, slug=None):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	instance = get_object_or_404(Post, slug=slug)
 	instance.delete()
 	messages.success(request, "Successfully deleted")
